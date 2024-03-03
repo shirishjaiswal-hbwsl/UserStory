2886d698f9dcde8bb1765c62a5339db88c38f0e5
shirish <shirish.jaiswal@hbwsl.com>
2024-03-03 10:57:47 +0000

    User Story 6

---
+++ app/code/Shirish/US6/Plugin/DescriptionPlugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shirish\US6\Plugin;
+
+use Magento\Catalog\Block\Product\View\Description;
+
+
+class DescriptionPlugin
+{
+    public function afterGetProduct(Description $description, $result)
+    {
+        $result->setDescription("Description of the product is this");
+        return $result;
+    }
+}
